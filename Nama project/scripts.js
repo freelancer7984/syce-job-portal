99cbbe12be701b2435356b1fe761dedc4a0e454f
freelancer7984 <freelancer7984@gmail.com>
2026-01-21 15:42:20 +0530

    Delete Nama project directory

--- Nama project/scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("searchForm").addEventListener("submit", function(e) {
-e.preventDefault();
-const search = document.getElementById("searchInput").value;
-window.location.href = "jobs.html?search=" + search;
-});
