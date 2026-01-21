ff564d589fcbe9d0b13c59ef101a27c627c86f46
freelancer7984 <freelancer7984@gmail.com>
2026-01-21 17:46:27 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,93 +1,29 @@
-body {
-  margin: 0;
-  font-family: Arial, sans-serif;
-  background: #f4f6f8;
+// SEARCH
+const searchForm = document.getElementById("searchForm");
+if (searchForm) {
+  searchForm.addEventListener("submit", e => {
+    e.preventDefault();
+    window.location.href = "jobs.html";
+  });
 }
 
-.header {
-  background: #0a66c2;
-  padding: 15px 30px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+// LOGIN
+const loginForm = document.getElementById("loginForm");
+if (loginForm) {
+  loginForm.addEventListener("submit", e => {
+    e.preventDefault();
 
-.logo {
-  color: white;
-  font-size: 22px;
-  font-weight: bold;
-}
-.logo span {
-  color: #ffdd00;
-}
+    const u = username.value;
+    const p = password.value;
+    const msg = document.getElementById("loginMsg");
 
-.header nav a {
-  color: white;
-  margin-left: 20px;
-  text-decoration: none;
-}
-
-.hero {
-  text-align: center;
-  padding: 80px 20px;
-  background: white;
-}
-
-.hero h1 span {
-  color: #0a66c2;
-}
-
-.search-box {
-  margin-top: 20px;
-}
-
-.search-box input {
-  padding: 12px;
-  width: 260px;
-}
-
-.search-box button {
-  padding: 12px 20px;
-  background: #0a66c2;
-  color: white;
-  border: none;
-}
-
-.jobs-section {
-  padding: 30px;
-  max-width: 700px;
-  margin: auto;
-}
-
-.job-card {
-  background: white;
-  padding: 20px;
-  margin-bottom: 15px;
-  border-radius: 6px;
-}
-
-.job-card button {
-  background: #0a66c2;
-  color: white;
-  border: none;
-  padding: 8px 14px;
-}
-
-.login-box {
-  background: white;
-  width: 320px;
-  margin: 120px auto;
-  padding: 25px;
-  text-align: center;
-  border-radius: 6px;
-}
-
-.login-box input {
-  width: 90%;
-  padding: 10px;
-  margin: 10px 0;
-}
-
-#loginMsg {
-  margin-top: 10px;
+    if (u === "student" && p === "1234") {
+      msg.style.color = "green";
+      msg.innerText = "Login successful!";
+      setTimeout(() => location.href = "jobs.html", 1000);
+    } else {
+      msg.style.color = "red";
+      msg.innerText = "Invalid credentials";
+    }
+  });
 }
